41700542c57cf2b957edcff9e89e11b3a9f51586
Sundeep Malladi <sundeep.malladi@gmail.com>
2013-11-06 14:41:58 -0600

    Placeholder behavior

--- inputosaurus.js
+++ inputosaurus.js
@@ -55,7 +55,10 @@
 
 			// manipulate and return the input value after parseInput() parsing
 			// the array of tag names is passed and expected to be returned as an array after manipulation
-			parseHook : null
+			parseHook : null,
+			
+			// define a placeholder to display when the input is empty
+			placeholder: null,
 		},
 
 		_create: function() {
@@ -70,6 +73,13 @@
 			els.input = $('<input type="text" />');
 			els.inputCont = $('<li class="inputosaurus-input inputosaurus-required"></li>');
 			els.origInputCont = $('<li class="inputosaurus-input-hidden inputosaurus-required">');
+
+			if (o.placeholder) { 
+				els.input.attr('placeholder', o.placeholder); 
+				if (o.width) {
+					els.input.css('min-width', o.width - 50);
+				}
+			}
 
 			o.wrapperElement && o.wrapperElement.append(els.ul);
 			this.element.replaceWith(o.wrapperElement || els.ul);
@@ -176,6 +186,8 @@
 				widget.elements.input.val('');
 				widget._resizeInput();
 			}
+
+			widget._resetPlaceholder();
 		},
 
 		_inputFocus : function(ev) {
@@ -217,6 +229,18 @@
 				txtWidth = 25 + val.length * 8;
 
 			widget.elements.input.width(txtWidth < maxWidth ? txtWidth : maxWidth);
+		},
+		
+		// resets placeholder on representative input
+		_resetPlaceholder: function () {
+			var placeholder = this.options.placeholder,
+				input = this.elements.input,
+				width = this.options.width || 'inherit';
+			if (placeholder && this.element.val().length === 0) {
+				input.attr('placeholder', placeholder).css('min-width', width - 50)
+			}else {
+				input.attr('placeholder', '').css('min-width', 'inherit')
+			}
 		},
 
 		// if our input contains no value and backspace has been pressed, select the last tag
